fa4b28139d3254906cdc30f950802e1251f1b947
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2021-04-10 01:43:00 +0300

    feat: solve 11-encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,28 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let array = str.split('');
+  let result = [];
+  for (let i = 1; i < array.length; i++) {
+    if (i === array.length - 1 && array[i] === array[i - 1]) {
+      result.push(array.length);
+      result.push(array[i]);
+    }
+    if (array[i] !== array[i - 1]) {
+      if (i !== 1) {
+        result.push(i);
+      }
+      result.push(array[i - 1]);
+      array = array.slice(i, array.length);
+      i = 0;
+      if (array.length === 1) {
+        result.push(array[0]);
+      }
+    }
+  }
+  result = result.map((e) => String(e)).join('');
+  return result;
 }
 
 module.exports = encodeLine;
