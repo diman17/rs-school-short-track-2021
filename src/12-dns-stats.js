c8a277e4d6a995d3edf2a8f536389c1278c9fd41
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2021-04-10 18:35:35 +0300

    feat: solve 12-dns-stats task

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,34 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  if (domains.length === 0) {
+    return {};
+  }
+  const arr = [...domains];
+  const result = {};
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].split('.').reverse();
+
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = `.${arr[i][j]}`;
+    }
+
+    let key = arr[i].join('');
+
+    if (arr[i].length > 2) {
+      result[key] = 1;
+    }
+
+    if (arr[i].length === 2) {
+      [key] = [arr[0][0]];
+      result[key] = domains.length;
+      key = arr[i].join('');
+      result[key] = domains.length;
+    }
+  }
+
+  return result;
 }
 
 module.exports = getDNSStats;
